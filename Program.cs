2dc7f75146a0e475b2fe49a868ac3ea0c1b8411b
MuhDavin213 <davin.new58@gmail.com>
2023-04-05 10:24:27 +0700

    add for while

--- Program.cs
+++ Program.cs
@@ -120,6 +120,16 @@ namespace Exercise1
         public void buku(SqlConnection con)
         {
             SqlCommand cmd = new SqlCommand("Select * FROM HRD", con);
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                for (int i = 0; i < r.FieldCount; i++)
+                {
+                    Console.WriteLine(r.GetValue(i));
+                }
+                Console.WriteLine();
+            }
         }
+
     }
 }
