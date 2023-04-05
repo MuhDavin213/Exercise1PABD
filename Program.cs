c71b9b16415a6af9fd265b7c8ef4fdf5f3359d84
MuhDavin213 <davin.new58@gmail.com>
2023-04-05 10:08:32 +0700

    add try program with console and type data

--- Program.cs
+++ Program.cs
@@ -10,7 +10,22 @@ namespace Exercise1
     {
         static void Main(string[] args)
         {
-
+            Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("koneksi ke database\n");
+                    Console.WriteLine("Masukkan User ID :");
+                    String user = Console.ReadLine();
+                    Console.WriteLine("masukkan password");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("masukkan database tujuan");
+                    string db = Console.ReadLine();
+                    Console.Write("\nKetik K untuk terhubung ke Database: ");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                }
+            }
         }
     }
 }
